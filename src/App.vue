a180db69ce339f090faf87c5b7aaccf05bacc7e1
Grant <grantshoop@gmail.com>
2020-03-16 20:52:26 -0600

    added router to project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
+    
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header></app-header>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/header.vue';
+
 
 export default {
+ 
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    appHeader: Header
   }
 }
 </script>
